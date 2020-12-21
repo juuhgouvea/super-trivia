95d21cca869c6e9fdd5c339643418b845af65875
juuhgouvea <juliasantos752@yahoo.com.br>
2020-12-21 01:29:27 -0300

    save game config to shared preferences

--- app/src/main/java/com/juuhgouvea/supertrivia/fragments/ConfigGameFragment.kt
+++ app/src/main/java/com/juuhgouvea/supertrivia/fragments/ConfigGameFragment.kt
@@ -1,18 +1,23 @@
 package com.juuhgouvea.supertrivia.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.juuhgouvea.supertrivia.R
 import com.juuhgouvea.supertrivia.adapters.CategoryAdapter
+import com.juuhgouvea.supertrivia.models.Category
 import com.juuhgouvea.supertrivia.models.Difficulty
 import kotlinx.android.synthetic.main.fragment_config_game.view.*
 
 class ConfigGameFragment : Fragment() {
+    private val categoryAdapter = CategoryAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,10 +29,35 @@ class ConfigGameFragment : Fragment() {
             view.selectDifficulty.adapter = adapter
         }
 
-        view.listCategories.adapter = CategoryAdapter()
+        view.listCategories.adapter = categoryAdapter
         view.listCategories.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        view.btnSaveConfig.setOnClickListener {
+            saveConfig(view.selectDifficulty.selectedItem as Difficulty, categoryAdapter.getSelectedCategory())
+            navigateToStartGame()
+        }
 
         return view
+    }
+
+    fun navigateToStartGame() {
+        findNavController().navigate(R.id.navigateToStartGame)
+    }
+
+    fun saveConfig(selectedDifficulty: Difficulty, selectedCategory: Category?) {
+        requireContext()
+            .getSharedPreferences("settings", Context.MODE_PRIVATE)
+            .edit().apply {
+                if(selectedCategory != null) {
+                    putLong("category_id", selectedCategory.id)
+                    putString("category_name", selectedCategory.name)
+                } else {
+                    remove("category_id")
+                    putString("category_name", "Random")
+                }
+                putString("difficulty", selectedDifficulty.value)
+                putString("difficulty_name", selectedDifficulty.name)
+                apply()
+            }
     }
 
     fun getDifficulties(): List<Difficulty> {
