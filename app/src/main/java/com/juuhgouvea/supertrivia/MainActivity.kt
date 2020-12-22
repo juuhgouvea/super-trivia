b8c062ba34b665d37b2d35cfa1eddfb4b5477831
juuhgouvea <juliasantos752@yahoo.com.br>
2020-12-21 22:02:18 -0300

    redirect to the correct fragment on main activity start

--- app/src/main/java/com/juuhgouvea/supertrivia/MainActivity.kt
+++ app/src/main/java/com/juuhgouvea/supertrivia/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.juuhgouvea.supertrivia
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(isConfigured()) {
+            if(isPlaying()) {
+                findNavController(R.id.navHostFragment).navigate(R.id.gameFragment)
+            } else {
+                findNavController(R.id.navHostFragment).navigate(R.id.startGameFragment)
+            }
+        }
+    }
+
+    fun isPlaying(): Boolean {
+        val gameStatus = this
+                .getSharedPreferences("game", Context.MODE_PRIVATE)
+                .getString("status", "")
+
+        return gameStatus!!.equals("playing")
+    }
+
+    fun isConfigured(): Boolean {
+        val categoryName = this
+                .getSharedPreferences("settings", Context.MODE_PRIVATE)
+                .getString("category_name", "")
+
+        val difficulty = this
+                .getSharedPreferences("settings", Context.MODE_PRIVATE)
+                .getString("difficulty", "")
+
+        return !categoryName!!.isEmpty() && !difficulty!!.isEmpty()
     }
 }
